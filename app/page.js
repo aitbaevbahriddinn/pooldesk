e5ecd3a74ab6416e74ef89930d757a51a997234a
aitbaevbahriddinn <aitbaevbahriddinn@gmail.com>
2026-07-27 10:56:39 +0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>PoolDesk</h1>
+      <p>Сайт работает. Дальше добавляем функции.</p>
+    </main>
+  )
+}
